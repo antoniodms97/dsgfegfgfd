50a6a3e1fbedc94b8b2ebe18481824635a8ee690
Antonio Sasso <antoniodsasso@gmail.com>
2022-10-08 13:30:50 -0700

    week 2 assignment

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -1,6 +1,19 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { baseUrl } from '../../shared/baseUrl';
 
+export const postComment = createAsyncThunk(
+    'comments/postComment',
+    async (payload, {dispatch,getState}) => {
+        setTimeout(() => {
+            const {comments} = getState();
+            payload.date = new Date().toISOString();
+            payload.id = comments.commentsArray.length;
+            dispatch(addComment(payload));
+            console.log(payload);
+        }, 2000);
+    }
+)
+
 export const fetchComments = createAsyncThunk(
     'comments/fetchComments',
     async () => {
@@ -12,7 +25,12 @@ export const fetchComments = createAsyncThunk(
 const commentsSlice = createSlice({
     name: 'comments',
     initialState: { isLoading: true, errMess: null, commentsArray: [] },
-    reducers: {},
+    reducers: {
+        addComment: (state, action) => {
+                state.commentsArray.push(action.payload);
+        }
+
+    },
     extraReducers: {
         [fetchComments.pending]: (state) => {
             state.isLoading = true;
@@ -28,5 +46,7 @@ const commentsSlice = createSlice({
         }
     }
 });
+
+export const { addComment } = commentsSlice.actions;
 
 export const commentsReducer = commentsSlice.reducer;
